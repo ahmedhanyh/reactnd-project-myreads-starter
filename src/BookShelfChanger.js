4e7fd465cdec0b7ed87cc4481a2c4533ecc3875a
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-11-22 23:30:47 +0200

    Changed defaultValue prop

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -4,7 +4,7 @@ class BookShelfChanger extends Component {
     render() {
         return (
             <div className="book-shelf-changer">
-                <select defaultValue={this.props.shelfName} onChange={e => this.props.onUpdateBookShelf(e.target.value)}>
+                <select defaultValue={this.props.shelfName ? this.props.shelfName : "none"} onChange={e => this.props.onUpdateBookShelf(e.target.value)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
